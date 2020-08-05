bba32a6d67b0620be2d742bcd8efeaa3dbffff31
rferrari81 <rferrari81@users.noreply.github.com>
2020-08-05 22:30:46 +0200

    Add files via upload

--- RetroPie-OLED.py
+++ RetroPie-OLED.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Title          : RetroPie-OLED.py
-Author         : zzeromin, losernator and members of Tentacle Team
-Creation Date  : Nov 13, 2016
-Thanks to      : smyani, zerocool, GreatKStar and members of Raspigamer Cafe.
+Title        : RetroPie_OLED.py
+Author       : zzeromin, losernator and members of Tentacle Team
+Creation Date: Nov 13, 2016
+Cafe         : http://cafe.naver.com/raspigamer
+Thanks to    : smyani, zerocool, GreatKStar and members of Raspigamer Cafe.
 Free and open for all to use. But put credit where credit is due.
-MOD version by : Roby F.
-Release        : 02-02-2020
 
-Reference      :
+Reference    :
 https://github.com/haven-jeon/piAu_volumio
 https://github.com/adafruit/Adafruit_CircuitPython_SSD1306
 https://pillow.readthedocs.io/en/stable/
@@ -29,6 +28,7 @@ import textwrap
 import busio
 import adafruit_ssd1306
 import subprocess
+import os
 
 from sys import exit
 from subprocess import *
@@ -37,7 +37,6 @@ from datetime import datetime
 from random import randint
 from PIL import Image, ImageDraw, ImageFont
 from board import SCL, SDA
-#from board import SCL, SDA
 
 # Create the I2C interface.
 i2c = busio.I2C(SCL, SDA)
@@ -64,28 +63,23 @@ def run_cmd(cmd):
     output = p.communicate()[0]
     return output
 
-def get_ip_address(cmd, cmdeth):
-    # ip & date information
-    ipaddr = run_cmd(cmd)
-
-    # selection of wlan or eth address
-    count = len(ipaddr)
-    if count == 0 :
-        ipaddr = run_cmd(cmdeth)
-    return ipaddr
-
 def get_cpu_temp():
     tempFile = open("/sys/class/thermal/thermal_zone0/temp")
     cpu_temp = tempFile.read()
     tempFile.close()
     return float(cpu_temp)/1000
-
+    
+def get_gpu_temp():
+        temp = os.popen("vcgencmd measure_temp").readline()
+        temp = temp[:-3]
+        return (temp.replace("temp=",""))
+        
 def get_cpu_speed():
     tempFile = open("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq")
     cpu_speed = tempFile.read()
     tempFile.close()
     return float(cpu_speed)/1000
-
+    
 def main():
     global intro
     
@@ -118,40 +112,33 @@ def main():
     font_icon = ImageFont.truetype('/home/pi/RetroPie-OLED/fontawesome-webfont.ttf', 20)
     font_icon2 = ImageFont.truetype('/home/pi/RetroPie-OLED/fontawesome-webfont.ttf', 14)
     font_icon3 = ImageFont.truetype('/home/pi/RetroPie-OLED/fontawesome-webfont.ttf', 16)
-
-    #get ip address of eth0 connection
-    cmdeth = "ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
-    #get ip address of wlan0 connection
-    cmd = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
-    #cmd = "ip addr show wlan1 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
-
+    new_Speed = round(get_cpu_speed(),1)
     new_Temp = round(get_cpu_temp(),1)
-    #old_Speed = new_Speed = get_cpu_speed()
-
+    
     while True:
         # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
         cmd = "hostname -I | cut -d\' \' -f1"
         IP = subprocess.check_output(cmd, shell = True )
         cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
         CPU = subprocess.check_output(cmd, shell = True )
-        #cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-        cmd = "free -m | awk 'NR==2{printf \"%s MB\", $3}'"
+        cmd = "free -m | awk 'NR==2{printf \"%s\", $3}'"
         MemUsage = subprocess.check_output(cmd, shell = True )
-        cmd = "free -m | awk 'NR==2{printf \"%s MB\", $2}'"
+        cmd = "free -m | awk 'NR==2{printf \"%s\", $2}'"
         MemTotal = subprocess.check_output(cmd, shell = True )
         cmd = "free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2 }'"
         MemPercent = subprocess.check_output(cmd, shell = True )
-        cmd = "df -h | awk '$NF==\"/\"{printf \"%d GB\", $3}'"
+        cmd = "df -h | awk '$NF==\"/\"{printf \"%d\", $3}'"
         DiskFree = subprocess.check_output(cmd, shell = True )
-        cmd = "df -h | awk '$NF==\"/\"{printf \"%d GB\", $2}'"
+        cmd = "df -h | awk '$NF==\"/\"{printf \"%d\", $2}'"
         DiskUsed = subprocess.check_output(cmd, shell = True )
         cmd = "df -h | awk '$NF==\"/\"{printf \"%s\", $5}'"
         DiskPercent = subprocess.check_output(cmd, shell = True )
-        CPUTemp = str( new_Temp ) + chr(0xB0) +"C"
-        cmd = "vcgencmd get_config arm_freq"
-        CPUclock = subprocess.check_output(cmd, shell = True )
-        cmd = "vcgencmd get_config gpu_freq"
-        GPUclock = subprocess.check_output(cmd, shell = True )
+        #CPUTemp = str( new_Temp ) + chr(0xB0) +"C"
+        CPUTemp = str( new_Temp )
+        new_Speed = round(get_cpu_speed(),1)
+        CPUSpeed = str( new_Speed )
+        GPUTemp = (get_gpu_temp())
+        
         try:
             f = open('/dev/shm/runcommand.log', 'r', -1,"utf-8")
             # except FileNotFoundError:
@@ -160,34 +147,45 @@ def main():
                 titleimg = Image.open("/home/pi/RetroPie-OLED/RetropieLogo.png").convert('1')
                 raspyimg = Image.open("/home/pi/RetroPie-OLED/RaspberryLogo.png").convert('1')
                 infoimg = Image.open("/home/pi/RetroPie-OLED/SysInfo.png").convert('1')
+                retroarchimg = Image.open("/home/pi/RetroPie-OLED/RetroArchLogo.png").convert('1')
             except IOError:
-                ipaddr = get_ip_address(cmd, cmdeth)
-                ipaddr = ipaddr.decode('utf-8')
-                ipaddr = ipaddr.replace("\n","")
-                new_Temp = round(get_cpu_temp(),1)
-                info = str( new_Temp ) + chr(0xB0) +"C"
-
-                msg1 = "RetroPie"
-                msg2 = "EMULATIONSTATION"
-
-                msg1_size = draw.textsize(msg1, font=font_system)
-                msg2_size = draw.textsize(msg2, font=font_msg)
-
                 draw.rectangle((0,0,width,height), outline=0, fill=0)
-                draw.text(((width-msg1_size[0])/2, top), msg1, font=font_system, fill=255)
-                draw.text(((width-98)/2, top+18), msg2, font=font_msg, fill=255)
-                draw.text((96, top+54), info , font=fonte_rom, fill=255)
-                draw.text((0, top+54), ipaddr, font=fonte_rom, fill=255)
+                msg1 = "SYSTEM INFO"
+                msg1_size = draw.textsize(msg1, font=font)
+                draw.text(((width-msg1_size[0])/2, top), msg1, font=font, fill=255)
+                # Icons
+                # Icon temperator
+                draw.text((5, top+7),    chr(62152),  font=font_icon, fill=255)
+                # Icon CPU
+                draw.text((65, top+12), chr(61614),  font=font_icon2, fill=255)
+                # Icon memory
+                draw.text((65, top+33), chr(62171),  font=font_icon3, fill=255)
+                # Icon disk
+                draw.text((5, top+33), chr(61888),  font=font_icon2, fill=255)
+                # Icon Wifi
+                #draw.text((0, top+52), chr(61931),  font=font_icon2, fill=255)
+                    
+                draw.text((15, top+48),    str(IP,'utf-8'),               font=font_system, fill=255)
+                draw.text((20, top+8),     CPUTemp,                       font=font,        fill=255)
+                draw.text((45, top+8),     str('CPU'),                    font=font,        fill=255)
+                draw.text((20, top+18),    GPUTemp,                       font=font,        fill=255)
+                draw.text((45, top+18),    str('GPU'),                    font=font,        fill=255)                    
+                draw.text((82, top+8),     str(CPU,'utf-8'),              font=font,        fill=255)                    
+                draw.text((82, top+18),    CPUSpeed,                      font=font,        fill=255)
+                draw.text((20, top+29),    str(DiskFree,'utf-8'),         font=font,        fill=255)
+                draw.text((45, top+29),    str('GB'),                     font=font,        fill=255)                    
+                draw.text((20, top+39),    str(DiskUsed,'utf-8'),         font=font,        fill=255)
+                draw.text((45, top+39),    str('GB'),                     font=font,        fill=255)                    
+                draw.text((82, top+29),    str(MemUsage,'utf-8'),         font=font,        fill=255)
+                draw.text((111, top+29),   str('MB'),                     font=font,        fill=255)                    
+                draw.text((82, top+39),    str(MemTotal,'utf-8'),         font=font,        fill=255)
+                draw.text((111, top+39),   str('MB'),                     font=font,        fill=255)                    
 
                 oled.image(image)
                 oled.show()
-                sleep(3)
-                #break
+                sleep(.1)
                 pass
             else:
-                ipaddr = get_ip_address(cmd, cmdeth)
-                ipaddr = ipaddr.decode('utf-8')
-                ipaddr = ipaddr.replace("\n","")
                 draw.rectangle((0,0,width,height), outline=0, fill=0)
                 if intro == 0:
                     image.paste(raspyimg,(0,0))
@@ -197,33 +195,49 @@ def main():
                     sleep(3)
                 elif intro == 1:
                     draw.rectangle((0,0,width,height), outline=0, fill=0)
-                    image.paste(titleimg,(0,0))
+                    image.paste(retroarchimg,(0,0))
                     oled.image(image)
                     oled.show()
                     intro = 2
                     sleep(3)
                 elif intro == 2:
                     draw.rectangle((0,0,width,height), outline=0, fill=0)
+                    image.paste(titleimg,(0,0))
+                    oled.image(image)
+                    oled.show()
+                    intro = 3
+                    sleep(3)
+                elif intro == 3:
+                    draw.rectangle((0,0,width,height), outline=0, fill=0)
                     image.paste(infoimg,(0,0))
                     # Icons
                     # Icon temperator
                     draw.text((5, top+7),    chr(62152),  font=font_icon, fill=255)
+                    # Icon CPU
+                    draw.text((65, top+12), chr(61614),  font=font_icon2, fill=255)
                     # Icon memory
                     draw.text((65, top+33), chr(62171),  font=font_icon3, fill=255)
                     # Icon disk
                     draw.text((5, top+33), chr(61888),  font=font_icon2, fill=255)
                     # Icon Wifi
                     #draw.text((0, top+52), chr(61931),  font=font_icon2, fill=255)
+                    
+                    draw.text((15, top+48),    str(IP,'utf-8'),               font=font_system, fill=255)
+                    draw.text((20, top+8),     CPUTemp,                       font=font,        fill=255)
+                    draw.text((45, top+8),     str('CPU'),                    font=font,        fill=255)
+                    draw.text((20, top+18),    GPUTemp,                       font=font,        fill=255)
+                    draw.text((45, top+18),    str('GPU'),                    font=font,        fill=255)                    
+                    draw.text((82, top+8),     str(CPU,'utf-8'),              font=font,        fill=255)                    
+                    draw.text((82, top+18),    CPUSpeed,                      font=font,        fill=255)
+                    draw.text((20, top+29),    str(DiskFree,'utf-8'),         font=font,        fill=255)
+                    draw.text((45, top+29),    str('GB'),                     font=font,        fill=255)                    
+                    draw.text((20, top+39),    str(DiskUsed,'utf-8'),         font=font,        fill=255)
+                    draw.text((45, top+39),    str('GB'),                     font=font,        fill=255)                    
+                    draw.text((82, top+29),    str(MemUsage,'utf-8'),         font=font,        fill=255)
+                    draw.text((111, top+29),   str('MB'),                     font=font,        fill=255)                    
+                    draw.text((82, top+39),    str(MemTotal,'utf-8'),         font=font,        fill=255)
+                    draw.text((111, top+39),   str('MB'),                     font=font,        fill=255)                    
 
-                    draw.text((15, top+48),    str(IP,'utf-8'),  font=font_system, fill=255)
-                    draw.text((20, top+8),     str(CPU,'utf-8'), font=font, fill=255)
-                    draw.text((20, top+18),    CPUTemp , font=font, fill=255)
-                    draw.text((20, top+29),    str(DiskFree,'utf-8'),  font=font, fill=255)
-                    draw.text((20, top+39),    str(DiskUsed,'utf-8'),  font=font, fill=255)
-                    draw.text((82, top+29),    str(MemUsage,'utf-8'),  font=font, fill=255)
-                    draw.text((82, top+39),    str(MemTotal,'utf-8'),  font=font, fill=255)
-
-                    #draw.text((0, top+42),    str(CPUclock,'utf-8'),  font=font, fill=255)
                     oled.image(image)
                     oled.show()
                     sleep(.1)
@@ -262,9 +276,6 @@ def main():
             romfile = romfile.replace("\n","")
             f.close()
             new_Temp = round(get_cpu_temp(),1)
-            ipaddr = get_ip_address(cmd, cmdeth)
-            ipaddr = ipaddr.decode('utf-8')
-            ipaddr = ipaddr.replace("\n","")
             info = str( new_Temp ) + chr(0xB0) +"C"
 
             if game_length == 0 :
@@ -304,30 +315,36 @@ def main():
                 draw.rectangle((0,0,width,height), outline=0, fill=0 )
                 image.paste(titleimg,(0,0))
                 if system == "TURN OFF":
-                
-                    #image.paste(infoimg,(0,0))
+                    draw.rectangle((0,0,width,height), outline=0, fill=0)
+                    image.paste(infoimg,(0,0))
                     # Icons
                     # Icon temperator
                     draw.text((5, top+7),    chr(62152),  font=font_icon, fill=255)
+                    # Icon CPU
+                    draw.text((65, top+12), chr(61614),  font=font_icon2, fill=255)
                     # Icon memory
                     draw.text((65, top+33), chr(62171),  font=font_icon3, fill=255)
                     # Icon disk
                     draw.text((5, top+33), chr(61888),  font=font_icon2, fill=255)
                     # Icon Wifi
                     #draw.text((0, top+52), chr(61931),  font=font_icon2, fill=255)
-
-                    draw.text((15, top+48),    str(IP,'utf-8'),  font=font_system, fill=255)
-                    draw.text((20, top+8),     str(CPU,'utf-8'), font=font, fill=255)
-                    draw.text((20, top+18),    CPUTemp , font=font, fill=255)
-                    draw.text((20, top+29),    str(DiskFree,'utf-8'),  font=font, fill=255)
-                    draw.text((20, top+39),    str(DiskUsed,'utf-8'),  font=font, fill=255)
-                    draw.text((82, top+29),    str(MemUsage,'utf-8'),  font=font, fill=255)
-                    draw.text((82, top+39),    str(MemTotal,'utf-8'),  font=font, fill=255)
-
-                    #draw.text((0, top+42),    str(CPUclock,'utf-8'),  font=font, fill=255)
-                    #draw.text((0, top+44), datetime.now().strftime( "%b %d %H:%M" ), font=fonte_rom, fill=255)
-                    #draw.text((96, top+54), info , font=fonte_rom, fill=255)
-                    #draw.text((0, top+54), ipaddr, font=fonte_rom, fill=255)
+                    
+                    draw.text((15, top+48),    str(IP,'utf-8'),               font=font_system, fill=255)
+                    draw.text((20, top+8),     CPUTemp,                       font=font,        fill=255)
+                    draw.text((45, top+8),     str('CPU'),                    font=font,        fill=255)
+                    draw.text((20, top+18),    GPUTemp,                       font=font,        fill=255)
+                    draw.text((45, top+18),    str('GPU'),                    font=font,        fill=255)                    
+                    draw.text((82, top+8),     str(CPU,'utf-8'),              font=font,        fill=255)                    
+                    draw.text((82, top+18),    CPUSpeed,                      font=font,        fill=255)
+                    draw.text((20, top+29),    str(DiskFree,'utf-8'),         font=font,        fill=255)
+                    draw.text((45, top+29),    str('GB'),                     font=font,        fill=255)                    
+                    draw.text((20, top+39),    str(DiskUsed,'utf-8'),         font=font,        fill=255)
+                    draw.text((45, top+39),    str('GB'),                     font=font,        fill=255)                    
+                    draw.text((82, top+29),    str(MemUsage,'utf-8'),         font=font,        fill=255)
+                    draw.text((111, top+29),   str('MB'),                     font=font,        fill=255)                    
+                    draw.text((82, top+39),    str(MemTotal,'utf-8'),         font=font,        fill=255)
+                    draw.text((111, top+39),   str('MB'),                     font=font,        fill=255)
+                    
                 oled.image(image)
                 oled.show()
                 sleep(.1)
